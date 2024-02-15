8a7023a01fc97e02e2cd3bcf719964a1174217c1
vuckoviic <vuckovica64@gmail.com>
2024-02-15 11:38:13 +0100

    Make About page working

--- src/about.js
+++ src/about.js
@@ -1,3 +1,18 @@
+import cook from "./cook.jpg"
+
 export function aboutLoad() {
-    
+    const h1 = document.createElement("h1");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+
+    h1.innerText = "The First Ever Italian Restaurant In USA";
+    img.src = cook;
+    p.innerText = "This is the first ever oldfashioned, old school restaurant in the center of New York. It is honor be a chief at this beautiful place";
+
+    const content = document.getElementById("content");
+
+    content.appendChild(h1);
+    content.appendChild(img);
+    content.appendChild(p);
+
 }
--- src/menu.js
+++ src/menu.js
@@ -3,8 +3,8 @@ export function menuLoad() {
     const img = document.createElement("img");
     const p = document.createElement("p");
 
-    h1.innerText = "The First Ever Italian Restaurant In USA";
-    img.src = restaurantImage;
+    h1.innerText = "Explore Italian National Cuisine";
+    img.src = cook;
     p.innerText = "This is the first ever oldfashioned, old school restaurant in the center of New York. It is honor be a chief at this beautiful place";
 
     const content = document.getElementById("content");
@@ -12,5 +12,4 @@ export function menuLoad() {
     content.appendChild(h1);
     content.appendChild(img);
     content.appendChild(p);
-
 }
